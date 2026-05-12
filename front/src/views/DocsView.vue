3ea2407b39d5df51a59d0c9aa2bd9365f781024e
Victor Shumeyko <8049164+kv4nt@users.noreply.github.com>
2026-05-12 16:52:57 +0300

    fix: add missing Vue stub files referenced in router (AppLayout + 8 views)

---
+++ front/src/views/DocsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppLayout>
+    <div class="page">
+      <h1>Документация</h1>
+      <p>Страница в разработке</p>
+    </div>
+  </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from '@/components/AppLayout.vue'
+</script>
---
+++ front/src/views/ProductView.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppLayout>
+    <div class="page">
+      <h1>Товар #{{ $route.params.id }}</h1>
+      <p>Страница в разработке</p>
+    </div>
+  </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from '@/components/AppLayout.vue'
+</script>
